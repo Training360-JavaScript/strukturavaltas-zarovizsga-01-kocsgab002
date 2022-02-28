aa805d21bf8648545516c74ceb59854a5b069627
Kocsis Gábor <ifjkocsis.gabor@gmail.com>
2022-02-28 14:21:10 +0100

    5. feladat kész

---
+++ firebase-5/src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAYL0ZYmeoY_3StJ-486BeshrdOh6lCkdY",
+  authDomain: "fir-bd688.firebaseapp.com",
+  databaseURL: "https://fir-bd688-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "fir-bd688",
+  storageBucket: "fir-bd688.appspot.com",
+  messagingSenderId: "316559741045",
+  appId: "1:316559741045:web:b7d5d183b0c2db10ce283c",
+  measurementId: "G-6RH18NXH6P"
+};
+
+
+export const environment = {
+  firebaseConfig,
+  production: true
+};
---
+++ firebase-5/src/environments/environment.ts
@@ -0,0 +1,29 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAYL0ZYmeoY_3StJ-486BeshrdOh6lCkdY",
+  authDomain: "fir-bd688.firebaseapp.com",
+  databaseURL: "https://fir-bd688-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "fir-bd688",
+  storageBucket: "fir-bd688.appspot.com",
+  messagingSenderId: "316559741045",
+  appId: "1:316559741045:web:b7d5d183b0c2db10ce283c",
+  measurementId: "G-6RH18NXH6P"
+};
+
+
+export const environment = {
+  firebaseConfig,
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
